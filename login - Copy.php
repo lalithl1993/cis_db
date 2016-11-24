7d6377765626b4d94e5f710214d603fbef9ddfe5
lalithl1993 <lalithl1993@gmail.com>
2016-11-24 11:57:38 +0530

    Add files via upload

--- login - Copy.php
+++ login - Copy.php
@@ -7,9 +7,9 @@ $password = $_POST['password'];
 require_once('dbConnect.php');
 $sql = "SELECT * FROM $table WHERE email = '$email' AND password='$password'";
 $result = mysqli_query($con,$sql);
-if($row = mysqli_fetch_assoc($result))
-{
-	echo $row['id'];
+$check = mysqli_fetch_array($result);
+if(isset($check)){
+echo 'success';
 }else{
 echo 'failure';
 }
